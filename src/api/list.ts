529b743aa646607a905b753e3248d253bb210662
nightrain <148872666+Nightrain0@users.noreply.github.com>
2026-05-27 21:54:29 +0800

    Update list.ts

--- src/api/list.ts
+++ src/api/list.ts
@@ -1,7 +1,12 @@
 import type { ListObjectsV2Output } from '@aws-sdk/client-s3';
 import axios from 'axios';
 
-export const ListFiles = async (MaxKeys?: string, Prefix?: string, ContinuationToken?: string): Promise<ListObjectsV2Output> => {
+export const ListFiles = async (
+    MaxKeys?: string, 
+    Prefix?: string, 
+    ContinuationToken?: string,
+    Delimiter?: string // 新增分隔符参数
+): Promise<ListObjectsV2Output> => {
     const endpoint = "/api/list";
     const params = new URLSearchParams();
     if (MaxKeys) {
@@ -12,6 +17,9 @@ export const ListFiles = async (MaxKeys?: string, Prefix?: string, ContinuationT
     }
     if (ContinuationToken) {
         params.append("ContinuationToken", ContinuationToken);
+    }
+    if (Delimiter) {
+        params.append("Delimiter", Delimiter);
     }
     const res = await axios.get(endpoint, {
         params,
