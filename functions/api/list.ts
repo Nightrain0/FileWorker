9191969dd94eb2effc4a8545adc5dad3700910b4
nightrain <148872666+Nightrain0@users.noreply.github.com>
2026-05-27 21:53:48 +0800

    Update list.ts

--- functions/api/list.ts
+++ functions/api/list.ts
@@ -3,21 +3,22 @@ import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import Env from "../utils/Env";
 import { createS3Client } from "../utils/utils";
 
-
 export const onRequestGet: PagesFunction<Env> = async (context) => {
     const { env, request } = context;
     const { BUCKET } = env;
     const querys = new URL(request.url).searchParams;
     const maxKeys = querys.get("MaxKeys");
-    const prefix = querys.get("Prefix");
+    const prefix = querys.get("Prefix") || undefined;
     const continuationToken = querys.get("ContinuationToken");
+    const delimiter = querys.get("Delimiter") || undefined; // 新增分隔符支持
 
     const s3 = createS3Client(env);
     const command = new ListObjectsV2Command({
         Bucket: BUCKET!,
         MaxKeys: maxKeys ? parseInt(maxKeys) : undefined,
         Prefix: prefix,
-        ContinuationToken: continuationToken
+        ContinuationToken: continuationToken,
+        Delimiter: delimiter // 传递给 AWS SDK
     });
     let response;
     try {
